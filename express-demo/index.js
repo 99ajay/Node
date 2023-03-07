cca1fd51f81d983651d4a869785c636a4b25f190
AJAY VERMA <tigersis1995@gmail.com>
2023-03-07 14:29:59 +0530

    handling http get request

--- express-demo/index.js
+++ express-demo/index.js
@@ -2,10 +2,7 @@ const express=require('express');
 
 const app=express();
 
- 
-// app.post();
-// app.put();
-// app.delete();
+ const courses=[{id: 1,name: 'course1'},{id: 2,name: 'corse2'},{id: 3,name: 'course3'}];
 
 app.get('/', (req,res)=>{
 res.send("hello world!!!");
@@ -17,8 +14,13 @@ res.send([1,2,3]);
 
 // /api/courses/1
 
-app.get('/api/posts/:year/:month',(req,res)=>{
- res.send(req.params);
+app.get('/api/posts/:id',(req,res)=>{
+ const course=courses.find(c=>c.id===parseInt(req.params.id));
+ if(!course) //404
+ {
+   res.status(404).send('the course with given id was not find');
+ }
+ res.send(course);
 });
 
 
