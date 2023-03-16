a09bcec6f34489c28ce4f742f8a3f74055989229
AJAY VERMA <tigersis1995@gmail.com>
2023-03-16 18:01:12 +0530

    post has been done

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express=require("express");
 //you hav eto write it-> app signifies ->your server
 
 const app=express();
+app.use(express.json());///it is necessary if it does not show then it will undefined 
 
 // app.get("/sayHello",function(req,res){ //forward slash mean anything from the home page 
 
@@ -19,6 +20,17 @@ const app=express();
 //   console.log(req.body);
 //   res.send("post wala hello ");
 // });
+let user;
+
+app.post("/sayHello/",function(req,res){
+//  console.log(req.body);
+//  res.send("hello from the post ");
+user =req.body;
+res.json({
+    message:"message send successfully ",
+    user:user
+})
+})
 app.get("/getMultiply/:num1/:num2/",function(req,res){  ///sending parameter as a request 
     
     console.log(req.params)
