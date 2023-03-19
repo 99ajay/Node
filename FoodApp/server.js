6df04382b27e0a0812986301da319ec1daa8c7ac
AJAY VERMA <tigersis1995@gmail.com>
2023-03-20 00:10:01 +0530

    schema model document mongodb

--- FoodApp/server.js
+++ FoodApp/server.js
@@ -1,9 +1,33 @@
 const express=require("express");
+const {Schema} = require("mongoose");
+const mongoose=require('mongoose');
 
 const app=express();
+app.use(express.json());
 
 const userModel=require("./userModel");
+
+//sighup input:
+//name
+//password
+//confirm password
+//address
+//phone
+//pic
+
+app.post("/signup",async function(req,res){
+    let data=req.body;
+    console.log(data);
+    let newUser=await userModel.create(data);
+    res.json({
+        message:"data received",
+        data:data
+    })
+})
 app.listen(3000,function(){
     console.log("server is started listening at 3000 port");
 
 })
+
+//
+ 
--- FoodApp/userModel.js
+++ FoodApp/userModel.js
@@ -1,4 +1,5 @@
 const mongoose=require('mongoose');
+const {Schema} = require("mongoose");
 
 let dblink=`mongodb+srv://dbUser:0FsmgjbR0H5o6Ug7@cluster0.jnvtpim.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -7,3 +8,37 @@ mongoose.connect(dblink).then(function(){
 }).catch(function(err){
     console.log("error",err);
 })
+
+
+let userSchema=new Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    confirmPassword:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    phoneno:{
+     type:String,
+     minLength:10,
+     maxLength:10
+    },
+    pic:{
+        type:String,
+        default:""
+    },
+})
+
+const userModel=mongoose.model('FoodUserModel',userSchema);
+
+module.exports=userModel;
